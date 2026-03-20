f814e61e3ff22a25b320dba408f9c2387833cbf4
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-03-20 12:57:23 +0900

    update code to meet requirements

--- src/Step2/Calculator.java
+++ src/Step2/Calculator.java
@@ -1,18 +1,22 @@
 package Step2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
-    int result;
+    List<Integer> results = new ArrayList<Integer>();
+    int result = 0;
 
     public int calculate(int operand1, int operand2, char operator) {
         switch (operator) {
-            case '+' -> result = operand1 + operand2;
-            case '-' -> result = operand1 - operand2;
-            case '*' -> result = operand1 * operand2;
+            case '+' -> results.add(result = operand1 + operand2);
+            case '-' -> results.add(result = operand1 - operand2);
+            case '*' -> results.add(result = operand1 * operand2);
             case '/' -> {
                 if (operand2 == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 } else {
-                    result = operand2 / operand1;
+                    results.add(result = operand1 / operand2);
                 }
             }
         }
