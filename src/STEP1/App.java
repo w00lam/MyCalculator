05d5cd350397335b09f138c104c7fc28089a047d
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-03-20 11:35:30 +0900

    feat: add user input handling using Scanner

---
+++ src/STEP1/App.java
@@ -0,0 +1,19 @@
+package STEP1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int a = sc.nextInt();
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int b = sc.nextInt();
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char c = sc.next().charAt(0);
+    }
+}
